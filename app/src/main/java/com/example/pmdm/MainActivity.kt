4a7b2cf876960fa63bf1eb1736b96079df860c1b
alber <albertoag128@gmail.com>
2022-09-27 15:36:15 +0200

    First PMDM commit

--- app/src/main/java/com/example/pmdm/MainActivity.kt
+++ app/src/main/java/com/example/pmdm/MainActivity.kt
@@ -2,10 +2,6 @@ package com.example.pmdm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
