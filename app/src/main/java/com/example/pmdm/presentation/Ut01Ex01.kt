3690649623a9aa8c8fa8b0a189db854f004d4116
alber <albertoag128@gmail.com>
2022-09-27 16:52:17 +0200

    First exercise understanding the linear layout model as well as how to create new activities and linked them to a xml file

---
+++ app/src/main/java/com/example/pmdm/presentation/Ut01Ex01.kt
@@ -0,0 +1,12 @@
+package com.example.pmdm.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pmdm.R
+
+class Ut01Ex01 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01_ex01)
+    }
+}
