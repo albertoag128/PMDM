d5ce0568101033370c567d4c32f8e15135c0a8eb
albertoag128 <91797204+albertoag128@users.noreply.github.com>
2022-10-03 12:05:32 +0200

    Regular fries Linear Layout design with rating stars, share and like button

--- app/src/main/java/com/example/pmdm/presentation/ex01/Ut01ex01activity.kt
+++ app/src/main/java/com/example/pmdm/presentation/ex01/Ut01ex01activity.kt
@@ -1,12 +1,12 @@
-package com.example.pmdm.presentation
+package com.example.pmdm.presentation.ex01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pmdm.R
 
-class Ut01Ex01 : AppCompatActivity() {
+class Ut01ex01activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ut01_ex01)
+        setContentView(R.layout.activity_ut01ex02activity)
     }
 }
---
+++ app/src/main/java/com/example/pmdm/presentation/ex02/Ut01ex02activity.kt
@@ -0,0 +1,16 @@
+package com.example.pmdm.presentation.ex02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.pmdm.R
+import java.time.format.TextStyle
+
+class Ut01ex02activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01ex02activity)
+
+
+    }
+}
