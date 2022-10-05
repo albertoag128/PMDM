918ac8beff371de3072887d9967d60b1b8b1a450
albertoag128 <91797204+albertoag128@users.noreply.github.com>
2022-10-05 10:40:56 +0200

    Regular fries Relative and Linear Layout design with rating stars, share and like button

---
+++ app/src/main/java/com/example/pmdm/presentation/ex03/Ut01Ex03Activity.kt
@@ -0,0 +1,12 @@
+package com.example.pmdm.presentation.ex03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pmdm.R
+
+class Ut01Ex03Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01_ex03)
+    }
+}
