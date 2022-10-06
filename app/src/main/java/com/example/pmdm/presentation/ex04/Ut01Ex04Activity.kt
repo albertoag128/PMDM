86cfb1a76ef74d3e230ad846d3b48375cf7611c9
albertoag128 <91797204+albertoag128@users.noreply.github.com>
2022-10-06 12:41:04 +0200

    Imitating a card item based on an Avila en tapas card item using Linear Layout and Relative Layout

---
+++ app/src/main/java/com/example/pmdm/presentation/ex04/Ut01Ex04Activity.kt
@@ -0,0 +1,12 @@
+package com.example.pmdm.presentation.ex04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pmdm.R
+
+class Ut01Ex04Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01_ex04)
+    }
+}
